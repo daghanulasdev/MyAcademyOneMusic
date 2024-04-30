459c273008f6907fc40c0e4b93c45bb70f4de203
pxr <pxr@DESKTOP-BIP5GPD>
2024-04-30 20:55:47 +0300

    BL complete. add scopes on program.cs

---
+++ OneMusic.BusinessLayer/Concrete/SongManager.cs
@@ -0,0 +1,46 @@
+﻿using OneMusic.BusinessLayer.Abstract;
+using OneMusic.DataAccessLayer.Abstract;
+using OneMusic.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneMusic.BusinessLayer.Concrete
+{
+    public class SongManager : ISongService
+    {
+        private readonly ISongDal _songDal;
+
+        public SongManager(ISongDal songDal)
+        {
+            _songDal = songDal;
+        }
+
+        public void TCreate(Song entity)
+        {
+            _songDal.Create(entity);
+        }
+
+        public void TDelete(int id)
+        {
+            _songDal.Delete(id);
+        }
+
+        public Song TGetById(int id)
+        {
+            return _songDal.GetById(id);
+        }
+
+        public List<Song> TGetList()
+        {
+            return _songDal.GetList();
+        }
+
+        public void TUpdate(Song entity)
+        {
+            _songDal.Update(entity);
+        }
+    }
+}
--- OneMusic.WebUI/Program.cs
+++ OneMusic.WebUI/Program.cs
@@ -1,6 +1,34 @@
+using OneMusic.BusinessLayer.Abstract;
+using OneMusic.BusinessLayer.Concrete;
+using OneMusic.DataAccessLayer.Abstract;
+using OneMusic.DataAccessLayer.Concrete;
+using OneMusic.DataAccessLayer.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IAboutDal, EfAboutDal>();
+builder.Services.AddScoped<IAboutService, AboutManager>();
+
+builder.Services.AddScoped<IAlbumDal, EfAlbumDal>();
+builder.Services.AddScoped<IAlbumService, AlbumManager>();
+
+builder.Services.AddScoped<IBannerDal, EfBannerDal>();
+builder.Services.AddScoped<IBannerService, BannerManager>();
+
+builder.Services.AddScoped<IContactDal, EfContactDal>();
+builder.Services.AddScoped<IContactService, ContactManager>();
+
+builder.Services.AddScoped<IMessageDal, EfMessageDal>();
+builder.Services.AddScoped<IMessageService, MessageManager>();
+
+builder.Services.AddScoped<ISingerDal, EfSingerDal>();
+builder.Services.AddScoped<ISingerService, SingerManager>();
+
+builder.Services.AddScoped<ISongDal, EfSongDal>();
+builder.Services.AddScoped<ISongService, SongManager>();
+
+builder.Services.AddDbContext<OneMusicContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
